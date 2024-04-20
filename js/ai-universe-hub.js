4850a7c1b54e4982a0301c69ebf95132ab745c9f
webgurusam <shyamghosh666@gmail.com>
2024-04-20 09:37:03 +0600

    load ai tools data

--- js/ai-universe-hub.js
+++ js/ai-universe-hub.js
@@ -1,1 +1,8 @@
-console.log('ai-universe-hub.js')
+const loadAiToolsData = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/ai/tools');
+    const data = await res.json();
+    const tools = data.data;
+    console.log(tools);
+}
+
+loadAiToolsData();
